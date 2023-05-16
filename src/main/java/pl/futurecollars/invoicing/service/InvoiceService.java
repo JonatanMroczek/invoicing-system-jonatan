a6b45099c35d75d4550b434a0f55648b99d970a7
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2023-05-16 16:11:12 +0200

    Task 5 spring dependency injection (#8)
    * Add Spring dependency injection
    
    * Add Spring dependency injection
    
    * Add Spring dependency injection
    
    * Revert "Add Spring dependency injection"
    
    This reverts commit f11d0ea9d69e06b109dc7a5b18645511aff8b17b.
    
    * Fixes to line ending (Checkstyle)
    
    * Fixes to line ending (Checkstyle)
    
    * Fixes to line ending (Checkstyle)
    
    * Fixes to line ending (Checkstyle)
    
    * Fixes to line ending (Checkstyle)
    
    * Fixes to line ending (Checkstyle)
    
    * Fixes to line ending (Checkstyle)

--- src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -2,14 +2,17 @@ package pl.futurecollars.invoicing.service;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
+@Service
 public class InvoiceService {
 
     private final Database db;
 
-    public InvoiceService(Database db) {
+    public InvoiceService(@Qualifier("fileBasedDatabase") Database db) {
         this.db = db;
     }
 
