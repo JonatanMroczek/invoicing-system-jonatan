f09b2a66b3f8eb555f01e36fac7baa8e807334d5
Jonatan <jonatanmroczek@gmail.com>
2023-09-20 22:30:45 +0200

    Add  company.html with form to add companies and companies list

---
+++ src/main/resources/public/companies.js
@@ -0,0 +1,55 @@
+const loadCompanies = async () => {
+    const response = await fetch('http://localhost:7777/companies');
+    const companies = await response.json();
+
+    const companiesTable = document.getElementById("companiesTable")
+    companies.forEach(company => {
+        const row = companiesTable.insertRow(-1);
+
+        const taxIdCell = row.insertCell(0);
+        taxIdCell.innerText = company.taxIdentificationNumber;
+
+        const nameCell = row.insertCell(1);
+        nameCell.innerText = company.name;
+
+        const addressCell = row.insertCell(2);
+        addressCell.innerText = company.address;
+
+         const pensionInsuranceCell = row.insertCell(3);
+        pensionInsuranceCell.innerText = company.pensionInsurance;
+
+        const healthInsuranceCell = row.insertCell(4);
+        healthInsuranceCell.innerText = company.healthInsurance;
+    })
+}
+const serializeFormToJson = form => JSON.stringify(
+    Array.from(new FormData(form).entries())
+        .reduce((m, [key, value]) =>
+            Object.assign(m, {[key]: value}), {})
+);
+
+function handleAddCompanyFormSubmit() {
+    const form = $("#addCompanyForm");
+    form.on('submit', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'companies',
+            type: 'post',
+            contentType: 'application/json',
+            data: serializeFormToJson(this),
+            success: function (data) {
+                $("#companiesTable").find("tr:gt(0)").remove();
+                loadCompanies()
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                alert(errorThrown)
+            }
+        });
+    });
+}
+
+window.onload = function () {
+    loadCompanies();
+    handleAddCompanyFormSubmit()
+};
