f4d98ab3d4c2f7667510e6e1f4c3dedc700cc9d1
Jonatan <jonatanmroczek@gmail.com>
2023-09-23 19:23:08 +0200

    Added angular CRUD form

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Company } from "./company"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'invoicing-app';
+  companies: Company[] = [
+    new Company(
+      "111-222-22-22",
+      "ul.Jesionowa 2",
+      "First INC.",
+      22.22,
+      11.22
+    )
+  ];
+
+  newCompany: Company = new Company("", "", "", 0, 0);
+
+  addCompany() {
+    this.companies.push(this.newCompany);
+    this.newCompany = new Company("", "", "", 0, 0)
+  }
+  
+  deleteCompany(companyToDelete: Company){
+    this.companies = this.companies.filter(company => company !==companyToDelete )
+  }
+
+  triggerUpdate(company: Company){
+    company.editedCompany = new Company(
+      company.taxIdentificationNumber,
+      company.address,
+      company.name,
+      company.healthInsurance,
+      company.pensionInsurance
+    )
+    company.editMode = true
+  }
+
+  updateCompany(updatedCompany: Company){
+    updatedCompany.taxIdentificationNumber = updatedCompany.editedCompany.taxIdentificationNumber
+    updatedCompany.address = updatedCompany.editedCompany.address
+    updatedCompany.name = updatedCompany.editedCompany.name
+    updatedCompany.healthInsurance = updatedCompany.editedCompany.healthInsurance
+    updatedCompany.pensionInsurance = updatedCompany.editedCompany.pensionInsurance
+
+    updatedCompany.editMode = false;
+  }
+
+  cancelCompanyUpdate(company: Company){
+    company.editMode = false
+  }
 }
